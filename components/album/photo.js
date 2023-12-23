394f7b5afec45dcf20f6d0e24200c203796ff9d6
Gexianss <134065834+Gexianss@users.noreply.github.com>
2023-12-23 23:34:39 +0800

    modal ok props no

--- components/album/photo.js
+++ components/album/photo.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from './photo.module.css'
-import PictureModal from './picture-modal' 
+import PictureModal from './picture-modal'
 
 export default function PhotoList({ albumId }) {
   const [photos, setPhotos] = useState([])
-  const [selectedPhoto, setSelectedPhoto] = useState(null)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false)
+  }
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
@@ -13,34 +21,27 @@ export default function PhotoList({ albumId }) {
       .then((json) => setPhotos(json))
   }, [albumId])
 
-  const openModal = (photo) => {
-    setSelectedPhoto(photo)
-  }
-
-  const closeModal = () => {
-    setSelectedPhoto(null)
-  }
-
   return (
     <>
       <div className={styles.card_list}>
         {photos.map((photo) => (
-          // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-          <div key={photo.id} onClick={() => openModal(photo)}>
+          <div key={photo.id}>
             <Image
               src={photo.thumbnailUrl}
               alt={photo.title}
               width={150}
               height={150}
               className={styles.card}
+              onClick={openModal}
+            />
+            <PictureModal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              photo={photo}
             />
           </div>
         ))}
       </div>
-
-      {selectedPhoto && (
-        <PictureModal photo={selectedPhoto} onClose={closeModal} />
-      )}
     </>
   )
 }
--- components/album/picture-modal.js
+++ components/album/picture-modal.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
 import style from './pictureModal.module.css'
 import { LiaDownloadSolid } from 'react-icons/lia'
@@ -8,56 +6,83 @@ import { LiaInfoCircleSolid } from 'react-icons/lia'
 import { IoMdClose } from 'react-icons/io'
 import { GoHeart } from 'react-icons/go'
 import { LiaBookmarkSolid } from 'react-icons/lia'
-
+import ReactModal from 'react-modal'
 import Image from 'next/image'
 
-export default function PictureModal({ photo }) {
+export default function PictureModal({ isOpen, onRequestClose, photo }) {
+  const modalStyle = {
+    overlay: {
+      position: 'fixed',
+      top: 120,
+      left: -20,
+      backgroundColor: 'rgba(17,17,17, .8)',
+    },
+    content: {
+      position: 'absolute',
+      width: '97%',
+      left: 35,
+      border: '0px solid #ccc',
+      background: '#111',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '0px',
+      outline: 'none',
+      boxShadow: '0 0 20px 15px rgba(255, 255, 255, .1)',
+    },
+  }
   return (
     <>
-      <div>
-        <div className={style.modal_header}>
-          <div className={style.modal_header_icon}>
-            <p className={style.modal_header_p}>Download</p>
-            <LiaDownloadSolid color="white" size={20} />
-          </div>
-          <div className={style.modal_header_icon}>
-            <p className={style.modal_header_p}>Share</p>
-            <TiArrowForwardOutline color="white" size={20} />
-          </div>
-          <div className={style.modal_header_icon}>
-            <p className={style.modal_header_p}>Info</p>
-            <LiaInfoCircleSolid color="white" size={20} />
-          </div>
-          <div className={style.btn_close}>
-            <IoMdClose color="white" size={32} />
-          </div>
-        </div>
-
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        style={modalStyle}
+      >
         <div>
-          <Image
-            src={photo.thumbnailUrl}
-            alt={photo.title}
-            width={150}
-            height={150}
-          />
-        </div>
-        <div className={style.modal_footer}>
-          <div>
-            <p className={style.modal_footer_title}>Title</p>
-            <p className={style.modal_footer_p}>{photo.title}</p>
-          </div>
-          <div className={style.modal_footer_btnBox}>
-            <div className={style.modal_footer_btn}>
-              <p className={style.modal_footer_icon_p}>Like</p>
-              <GoHeart color="white" size={20} />
+          <div className={style.modal_header}>
+            <div className={style.modal_header_icon}>
+              <p className={style.modal_header_p}>Download</p>
+              <LiaDownloadSolid color="white" size={20} />
             </div>
-            <div className={style.modal_footer_btn}>
-              <p className={style.modal_footer_icon_p}>Save</p>
-              <LiaBookmarkSolid color="white" size={20} />
+            <div className={style.modal_header_icon}>
+              <p className={style.modal_header_p}>Share</p>
+              <TiArrowForwardOutline color="white" size={20} />
+            </div>
+            <div className={style.modal_header_icon}>
+              <p className={style.modal_header_p}>Info</p>
+              <LiaInfoCircleSolid color="white" size={20} />
+            </div>
+            <div className={style.btn_close}>
+              <IoMdClose color="white" size={32} onClick={onRequestClose} />
             </div>
           </div>
+
+          <div className={style.box_img}>
+            <Image
+              src={photo.thumbnailUrl}
+              alt={photo.title}
+              width={500}
+              height={500}
+              className={style.photo_img}
+            />
+          </div>
+          <div className={style.modal_footer}>
+            <div>
+              <p className={style.modal_footer_title}>Title</p>
+              <p className={style.modal_footer_p}>{photo.title}</p>
+            </div>
+            <div className={style.modal_footer_btnBox}>
+              <div className={style.modal_footer_btn}>
+                <p className={style.modal_footer_icon_p}>Like</p>
+                <GoHeart color="white" size={20} />
+              </div>
+              <div className={style.modal_footer_btn}>
+                <p className={style.modal_footer_icon_p}>Save</p>
+                <LiaBookmarkSolid color="white" size={20} />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </ReactModal>
     </>
   )
 }
